7b6b132e2e10d152abb795515fb6858f89995b32
d-e-h-i-o <niklas.dehio@gmail.com>
2021-05-27 12:29:10 +0200

    Add save_to_wayback_machine.py, get_plaintext.py, retries

--- annotation_pipline/fetch_annotations.py
+++ annotation_pipline/fetch_annotations.py
@@ -2,10 +2,12 @@ import json
 from typing import List, Dict
 
 from requests import get
+from retry import retry
 
 ANNOTATION_URL = "https://hypothes.is/api/search?limit=50&user=niklas_thesis"
 
 
+@retry(tries=3, delay=2)
 def fetch_annotations() -> List[Dict]:
     """TODO: Check how to fetch all annotations (can I just omit the limit?)"""
 
--- annotation_pipline/run_pipeline.py
+++ annotation_pipline/run_pipeline.py
@@ -1,9 +1,13 @@
 from .fetch_annotations import fetch_annotations
+from .save_to_wayback_machine import save_to_wayback_machine
 
 
 def run_pipeline():
     annotations = fetch_annotations()
 
+    for annotation in annotations:
+        save_to_wayback_machine(annotation["url"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run_pipeline()
