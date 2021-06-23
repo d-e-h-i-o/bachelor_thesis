6b6fa1ebd929f7463ffda55abdf891bea8bed301
d-e-h-i-o <niklas.dehio@gmail.com>
2021-06-23 15:50:51 +0200

    Add Annotation class

--- annotation_pipline/database.py
+++ annotation_pipline/database.py
@@ -1,47 +1,48 @@
 import sqlite3
 from datetime import datetime
 
+from annotation import Annotation
+
 connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
 
 today = datetime.today().date().strftime("%d.%m.%y")
 
 
-def not_yet_processed(annotation) -> bool:
+def not_yet_processed(raw_annotation) -> bool:
     """Returns false if the annotations is already in the database."""
-    cursor.execute("SELECT * FROM claims WHERE id = ?", (annotation["id"],))
+    cursor.execute("SELECT * FROM claims WHERE id = ?", (raw_annotation["id"],))
     return not bool(cursor.fetchone())
 
 
-def _save_extraction_data(_id, claim, url, plaintext) -> None:
+def _save_extraction_data(annotation, plaintext) -> None:
     cursor.execute(
         "INSERT INTO fulltext VALUES(?, ?, ?)",
-        (url, plaintext, today),
+        (annotation.url, plaintext, today),
     )
     cursor.execute(
         "INSERT INTO claims VALUES(?, ?, ?, ?)",
-        (_id, claim, url),
+        (annotation.id, annotation.claim, annotation.url),
     )
 
 
-def _save_matching_data(_id, reference, date) -> None:
+def _save_matching_data(annotation) -> None:
     cursor.execute(
         "INSERT INTO references VALUES(?, ?, ?)",
-        (_id, reference, date),
+        (annotation.id, annotation.reference, annotation.date),
     )
 
 
-def save_to_database(annotation, plaintext) -> None:
-    claim = annotation["target"][0]["selector"][2]["exact"]
-    if plaintext.find(claim) == -1:
+def save_to_database(raw_annotation, plaintext) -> None:
+    annotation = Annotation(raw_annotation)
+    if plaintext.find(annotation.claim) == -1:
         print(
-            f"For url {annotation['uri']}: Claim not found in plaintext. Plaintext has to be manually added to fulltext"
+            f"For url {annotation.url}: Claim not found in plaintext. Plaintext has to be manually added to fulltext"
             f"table."
         )
         plaintext = ""
 
-    _save_extraction_data(
-        annotation["id"], annotation["text"], annotation["uri"], plaintext
-    )
-    _save_matching_data(annotation["id"], annotation["text"], annotation["tags"][0])
+    _save_extraction_data(annotation, plaintext)
+    if annotation.for_law_matching and annotation.date:
+        _save_matching_data(annotation)
     connection.commit()
