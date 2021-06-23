512e99e4128de09a942f9fe90e91f6e26d3e3441
d-e-h-i-o <niklas.dehio@gmail.com>
2021-06-23 14:18:56 +0200

    Add pagination for annotation fetching

--- annotation_pipline/fetch_annotations.py
+++ annotation_pipline/fetch_annotations.py
@@ -4,13 +4,23 @@ from typing import List, Dict
 from requests import get
 from retry import retry
 
-ANNOTATION_URL = "https://hypothes.is/api/search?limit=50&user=niklas_thesis"
+ANNOTATION_URL = "https://hypothes.is/api/search?user=niklas_thesis"
 
 
 @retry(tries=3, delay=2)
 def fetch_annotations() -> List[Dict]:
-    """TODO: Check how to fetch all annotations (can I just omit the limit?)"""
 
-    r = get(ANNOTATION_URL)
-    annotations = json.loads(r.text)["rows"]
+    offset = 0
+    limit = 150
+    annotations = []
+    while True:
+        r = get(ANNOTATION_URL + f"&offset={offset}&limit={limit}")
+        rows = json.loads(r.text)["rows"]
+
+        if len(rows) == 0:
+            break
+
+        annotations = annotations + rows
+        offset += 150
+
     return annotations
