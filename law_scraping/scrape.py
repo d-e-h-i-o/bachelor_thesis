7bf64c381dbe220e84218e1f30a91b9e2e7348a4
d-e-h-i-o <niklas.dehio@gmail.com>
2021-07-20 22:49:28 +0200

    New scraped html pages
    There are some duplicates now in data/html_pages, since the format changed. Also some (the most recent, currently valid laws) have to be scraped by hand, since automatic extraction doesn't work for some reason

--- law_scraping/scrape.py
+++ law_scraping/scrape.py
@@ -5,21 +5,23 @@ import json
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from retry import retry
 
 random_sleep = lambda: sleep(randint(2, 7))
 
 
+@retry(tries=3, delay=2, jitter=(1, 3))
 def fetch_page(driver, url: str) -> Optional[str]:
-    try:
-        driver.get(url)
-        sleep(10)
-        return driver.page_source
-    except Exception as e:
-        print(e)
+    driver.get(url)
+    sleep(10)
+    return driver.page_source
 
 
-def save_page(html: str, url: str) -> None:
-    filename = url.split("-")[-1].replace("%", "_")
+def save_page(html: str, url: str, law: str) -> None:
+    if "%" in url:
+        filename = law + "#" + url.split("%")[-1]
+    else:
+        filename = law + "#"
 
     with open(f"data/html_pages/{filename}.html", "w+") as file:
         file.write(html)
@@ -30,13 +32,13 @@ if __name__ == "__main__":
     driver = webdriver.Chrome(ChromeDriverManager().install())
     failed = []
 
-    with open("data/urls/urls_berliner_verordnungen.json", "r") as file:
+    with open("data/urls/all_urls.json", "r") as file:
         to_scrape = json.load(file)
 
     for law in to_scrape:
         for version_url in to_scrape[law]:
             if html := fetch_page(driver, version_url):
-                save_page(html, version_url)
+                save_page(html, version_url, law)
             else:
                 failed.append(version_url)
             random_sleep()
