143eb987aadf279f8904c133329299041fb46d73
d-e-h-i-o <niklas.dehio@gmail.com>
2021-08-12 12:02:14 +0200

    Change epochs

--- training/train_claim_extraction_model.py
+++ training/train_claim_extraction_model.py
@@ -18,7 +18,7 @@ args = TrainingArguments(
     per_device_train_batch_size=4,
     per_device_eval_batch_size=4,
     per_gpu_train_batch_size=1,
-    num_train_epochs=3,
+    num_train_epochs=30,
     weight_decay=0.01,
 )
 
