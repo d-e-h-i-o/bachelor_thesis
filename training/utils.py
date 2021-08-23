5e6a620001becf26485fd4fc295a3afc7fc77156
d-e-h-i-o <niklas.dehio@gmail.com>
2021-08-23 13:46:15 +0200

    Generalize eval_k_fold in utils.py

--- training/utils.py
+++ training/utils.py
@@ -8,25 +8,17 @@ from datasets import load_metric
 def eval_k_fold(results):
     # TODO: Is this correct? Is there a better way to do this?
     num_results = len(results)
-    overall = {
-        "eval_loss": 0.0,
-        "eval_precision": 0.0,
-        "eval_recall": 0.0,
-        "eval_f1": 0.0,
-        "eval_accuracy": 0.0,
-    }
-    for result in results:
-        overall["eval_loss"] += result["eval_loss"]
-        overall["eval_precision"] += result["eval_precision"]
-        overall["eval_recall"] += result["eval_recall"]
-        overall["eval_f1"] += result["eval_f1"]
-        overall["eval_accuracy"] += result["eval_accuracy"]
+    keys = list(results[0])
+    overall = {}
+    for key in keys:
+        overall[key] = 0.0
 
-    overall["eval_loss"] /= num_results
-    overall["eval_precision"] /= num_results
-    overall["eval_recall"] /= num_results
-    overall["eval_f1"] /= num_results
-    overall["eval_accuracy"] /= num_results
+    for result in results:
+        for key in keys:
+            overall[key] += result[key]
+
+    for key in keys:
+        overall[key] /= num_results
 
     return overall
 
