1e4074aa329ab4b76bc9e00c01d8bd7a00cd71bb
d-e-h-i-o <niklas.dehio@gmail.com>
2021-11-07 01:32:59 +0100

    Add visuals for experiment1a.py

--- training/experiment1a.py
+++ training/experiment1a.py
@@ -3,6 +3,8 @@ import os
 from datetime import datetime
 from random import randint
 
+import jinja2
+import numpy as np
 from transformers import (
     AutoModelForTokenClassification,
     TrainingArguments,
@@ -19,6 +21,84 @@ from utils import (
     compute_metrics_claim_extraction,
     report_results,
 )
+
+
+def check_token(prediction, label):
+    if prediction == 0 and label == 0:
+        "Correctly predicted as not being part of a claim"
+        return "black"
+    if prediction == 1 and label == 1:
+        "Correctly predicted as the claim start"
+        return "green"
+    if prediction == 2 and label == 2:
+        "Correctly predicted as being part of the claim"
+        return "green"
+    if prediction == 1 and label == 2:
+        "Correctly predicted as claim, but should be inside of claim"
+        return "lightgreen"
+    if prediction == 2 and label == 1:
+        "Correctly predicted as claim, but should be new of claim"
+        return "darkgreen"
+    if prediction == 0 and label in [1, 2]:
+        "Incorrectly not labeled as claim"
+        return "red"
+    if prediction in [1, 2] and label == 0:
+        "Incorrectly labeled as claim"
+        return "blue"
+    return "white"
+
+
+def create_inspection_list(preds, labels, input_ids):
+    previous_token = None
+    chunks = []
+    current_chunk = None
+    for i in range(len(input_ids)):
+        current_token = check_token(preds[i], labels[i])
+
+        if previous_token is None or current_token != previous_token:
+            "New chunk"
+            if current_chunk is not None:
+                chunks.append(current_chunk)
+            current_chunk = (current_token, [input_ids[i]])
+        else:
+            current_chunk[1].append(input_ids[i])
+
+        previous_token = current_token
+
+    return chunks
+
+
+def transform_chunk(chunk, tokenizer):
+    label, input_ids = chunk
+    return (label, tokenizer.decode(input_ids))
+
+
+def render_html(list_of_samples, results):
+    TEMPLATE = """<div> {{ results }}</div>
+    {% for sample in list_of_samples -%}<p>
+    {% for chunk in sample -%}<span style="color: {{ chunk[0] }};"> {{ chunk[1] }} </span>{% endfor -%}</p><hr>
+    <p>{% for chunk in sample -%}\\highlight[green]{ {{ chunk[0] }} }[{{ chunk[1] }}]{% endfor -%}{% endfor -%}</p><hr>
+    """
+    return (
+        jinja2.Environment()
+        .from_string(TEMPLATE)
+        .render(list_of_samples=list_of_samples, results=results)
+    )
+
+
+def save_results(preds_labels_inputs_ids, results, tokenizer, name):
+    list_of_samples = map(
+        lambda chunks: map(lambda chunk: transform_chunk(chunk, tokenizer), chunks),
+        map(lambda t: create_inspection_list(*t), preds_labels_inputs_ids),
+    )
+    html = render_html(list_of_samples, results)
+    date = datetime.today().strftime("%d.%m.%y")
+    path = f"/data/experiments/dehio/bachelor_thesis/results/experiment1a_{date}"
+    with open(
+        f"{path}/visual_{name}.html",
+        "w+",
+    ) as file:
+        file.write(html)
 
 
 def run_experiment1a(
@@ -98,6 +178,16 @@ def run_experiment1a(
                 if run not in results[model_checkpoint]:
                     results[model_checkpoint][run] = []
                 results[model_checkpoint][run].append(result)
+
+                output = trainer.predict(test_dataset)
+                labels = output[1]
+                preds = np.argmax(output[0], axis=2)
+                save_results(
+                    zip(preds, labels, map(lambda x: x["input_ids"], test_dataset)),
+                    result,
+                    tokenizer,
+                    model_name + f"_fold{i}",
+                )
 
     date = datetime.today().strftime("%d.%m.%y")
     path = f"/data/experiments/dehio/bachelor_thesis/results/experiment1a_{date}"
